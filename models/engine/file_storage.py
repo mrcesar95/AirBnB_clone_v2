cda23754f7cedc3df675ebeffe7291df281a116b
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-14 10:52:30 -0500

    Adding setup_mysql_dev.sql, setup_mysql_test.sql, models/engine/file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,26 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        dictionary = {}
+        if cls:
+            for key, value in FileStorage.__objects.items():
+                if value.__class__ == cls:
+                    dictionary[key] = value
+            return dictionary
         return FileStorage.__objects
+
+    def delete(self, obj=None):
+        """Deletes an object from storage"""
+        if obj:
+            for key, value in FileStorage.__objects.items():
+                if value == obj:
+                    del FileStorage.__objects[key]
+                    self.save()
+                    return
+        else:
+            pass
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -26,7 +43,10 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Deserializes the JSON file to __objects
+		- only if the JSON file (__file_path) exists
+		- otherwise, do nothing.
+		- If the file doesn't exist, no exception should be raised."""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -36,15 +56,19 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, 'r', encoding='UTF-8') as file:
+                js = json.load(file)
+                for key, value in js.items():
+                    reloadobj = classes[js[key]['__class__']](**js[key])
+                    self.__objects[key] = reloadobj
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """Method for deserializing the JSON file to objects"""
+        self.reload()
