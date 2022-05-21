a4dd09545e1b103103584bd7c793186439a11e78
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-21 16:57:13 -0500

    Adding 2-do_deploy_web_static.py file

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# """Script that deploy an archive"""
+from fabric.api import env, local, run, put
+from datetime import datetime
+env.host = ['34.75.79.52', '54.85.108.121']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """Function that creates a .tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = 'versions/web_static_{}.tgz'.format(date)
+        local('mkdir -p versions')
+        local('tar -cvzf {} web_static'.format(file_name))
+        return file_name
+    except Exception as e:
+        return None
+
+
+def do_deploy(archive_path):
+    """Function that distributes an archive to your web servers"""
+    try:
+        filename = archive_path.split('/')[-1]
+        onlyname = filename.split('.')[0]
+        uncompress_path = "data/web_static/releases/{}".format(onlyname)
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p {}'.format(uncompress_path))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(filename, uncompress_path))
+        run('sudo rm /tmp/{}'.format(filename))
+        run('sudo mv {0}/web_static/* {0}/'.format(uncompress_path))
+        run('sudo rm -rf {0}/web_static'.format(uncompress_path))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(uncompress_path))
+        print('New version deployed!')
+        return True
+    except BaseException:
+        print('Do it again')
+        return False
