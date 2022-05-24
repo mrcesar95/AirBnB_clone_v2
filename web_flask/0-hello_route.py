1fea1164ab47da247b23bdad4c99e4ea08bfd327
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-24 11:38:34 -0500

    Fixing up pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-	"""Hello route Flask"""
-	return 'Hello HBNB!'
+    """Hello route Flask"""
+    return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
