27bc746d31d6200026e2776c6833b5db6544c2d4
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-24 14:52:03 -0500

    Adding web_flask/7-states_list.py, web_flask/templates/7-states_list.html files

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module flask"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """Close sqlalchemy session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """States list"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
