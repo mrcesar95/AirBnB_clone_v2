0f3bc17c6935e4d48124ea05fc4ab1224d45453a
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-24 17:25:58 -0500

    Fixing up 7-states_list.py file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module flask"""
+"""Module Flask"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,16 +9,16 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(self):
-    """Close sqlalchemy session"""
+    """Close SLQAlchemy session"""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def show_states_list():
-    """States list"""
+    """html that shows the list of states"""
     states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
+    return(render_template("7-states_list.html", states=states))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
