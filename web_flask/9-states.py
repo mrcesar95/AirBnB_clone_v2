5ed36e42032f3dd77a1bdfd446b14ee04c3ac5c9
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-25 07:04:09 -0500

    Fixing up web_flask/9-states.py, web_flask/templates/9-states.html files

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-""" flask """
+"""Module flask"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_db(self):
-    """Remove the current SQLAlchemy Session."""
+    """Remove the current SQLAlchemy session"""
     storage.close()
 
 
-@app.route('/states/', strict_slashes=False)
-def no_cities():
-    """states only"""
-    all_states = storage.all('State')
-    return render_template('9-states.html', states=all_states)
-
-
+@app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
-def state_and_cities(id):
-    """by id"""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+def states_and_cities(id=None):
+    """List all states"""
+    states = storage.all(State).values()
+    if id is not None:
+        for state in states:
+            if state.id == id:
+                return(render_template("9-states.html", states=state))
+        return(render_template("9-states.html"))
+    return(render_template("9-states.html", states=states, variable=True))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
