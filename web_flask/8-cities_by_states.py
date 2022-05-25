fc8cca68c0b19280637d21528dc380d5838b642a
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-25 06:39:26 -0500

    Fixing up web_flask/8-cities_by_states.py, web_flask/templates/8-cities_by_states.html files

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module flask"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,7 +9,7 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(self):
-    """Close SLQAlchemy session"""
+    """Close SQLAlchemy session"""
     storage.close()
 
 
@@ -16,8 +17,8 @@ def teardown_db(self):
 def show_states_list():
     """Show states list"""
     states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
+    return(render_template("8-cities_by_states.html", states=states))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
